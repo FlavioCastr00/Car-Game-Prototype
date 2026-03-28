4afcd67a62ade53c6d737c9048feb000ff42c54c
unknown <flaviohen1701@gmail.com>
2026-03-28 15:17:05 -0300

    Improve Inertia and Steering
    Changed linear and angular dumping for better inertia. Also implemented code that improves steering.

--- Car_Game_Prototype/Assets/Scripts/PlayerCarController.cs
+++ Car_Game_Prototype/Assets/Scripts/PlayerCarController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerCarController : MonoBehaviour
 {
     [SerializeField] private float accelerationForce = 30000f;
-    [SerializeField] private float turnVelocity = 750f;
+    [SerializeField] private float turnVelocity = 400f;
     [SerializeField] private float maxSpeed = 100f;
-    [SerializeField] private float sidewaysGrip = 5;
+    [SerializeField] private float sidewaysGrip = 10;
+    [SerializeField] private float linearDumping = 0.12f;
+    [SerializeField] private float angularDumping = 10f;
 
     private Rigidbody rb;
 
@@ -14,13 +16,14 @@ public class PlayerCarController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass = new Vector3(0, -0.5f, 0); // Lower center of mass to make car harder to flip
-        rb.linearDamping = 1.0f;
-        rb.angularDamping = 2.0f;
+        rb.linearDamping = linearDumping;
+        rb.angularDamping = angularDumping;
     }
 
     void FixedUpdate()
     {
         float speed = rb.linearVelocity.magnitude;
+        float forwardSpeed = Vector3.Dot(rb.linearVelocity, transform.forward);
 
         // Inputs
         float accelerationInput = Input.GetAxis("Vertical");
@@ -31,8 +34,15 @@ public class PlayerCarController : MonoBehaviour
         rb.AddForce(transform.forward * accelerationInput * accelerationForce * speedFactor);
 
         // Turn car
-        float turnAmount = turnInput * turnVelocity * Mathf.Clamp01(speed / maxSpeed) * Time.fixedDeltaTime;
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, turnAmount, 0f));
+        if (Mathf.Abs(forwardSpeed) > 0.03f)
+        {
+            float direction = Mathf.Sign(forwardSpeed); // 1 or -1
+            float turnAmount = turnInput * turnVelocity * Mathf.Clamp01(speed / maxSpeed) * direction * Time.fixedDeltaTime;
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, turnAmount, 0f));
+        }
+
+        // Dynamic Inertia
+        rb.linearDamping = linearDumping + (speed * 0.02f);
 
         // Lateral Friction
         Vector3 lateralVelocity = Vector3.Dot(rb.linearVelocity, transform.right) * transform.right;
