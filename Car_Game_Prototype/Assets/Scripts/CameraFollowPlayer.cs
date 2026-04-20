c9b691c77823c51a5f7b341f4e4c177fb833f65b
unknown <flaviohen1701@gmail.com>
2026-04-20 11:02:41 -0300

    Implement camera lag

--- Car_Game_Prototype/Assets/Scripts/CameraFollowPlayer.cs
+++ Car_Game_Prototype/Assets/Scripts/CameraFollowPlayer.cs
@@ -5,14 +5,21 @@ public class CameraFollowPlayer : MonoBehaviour
     public Transform cameraLookPosition;
 
     [SerializeField] private Vector3 cameraPos1 = new Vector3(0f, 1.52f, -3.5f);
+    [SerializeField] private float smoothTime = 0.1f;
+
+    private Vector3 velocity = Vector3.zero;
 
     // LateUpdate called after all Update functions
     void LateUpdate()
     {
-        // Camera offset position
-        Vector3 cameraPosition = cameraLookPosition.position + cameraLookPosition.rotation * cameraPos1;
-        transform.position = cameraPosition;
         // Camera focus point
         transform.LookAt(cameraLookPosition);
+    }
+
+    void FixedUpdate()
+    {
+        // Camera offset position
+        Vector3 targetPosition = cameraLookPosition.position + cameraLookPosition.rotation * cameraPos1;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
